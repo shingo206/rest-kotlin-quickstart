25e55ab1b09c58df6f98a9a48bf9311dfda74501
Shingo Tamura <shingo.tamura@nipponexpress.com>
2025-06-26 14:47:29 +0900

    Delete GreetingResourceIT.kt

--- src/main/kotlin/com/nipponexpress/domain/model/User.kt
+++ src/main/kotlin/com/nipponexpress/domain/model/User.kt
@@ -3,6 +3,8 @@ package com.nipponexpress.domain.model
 import io.quarkus.hibernate.reactive.panache.PanacheEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 
 @Entity
 data class User(
--- src/native-test/kotlin/com/nipponexpress/GreetingResourceIT.kt
+++
@@ -1,6 +0,0 @@
-package com.nipponexpress
-
-import io.quarkus.test.junit.QuarkusIntegrationTest
-
-@QuarkusIntegrationTest
-class GreetingResourceIT : GreetingResourceTest()
